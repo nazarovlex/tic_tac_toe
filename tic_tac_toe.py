4cb39f797361843576899b259d388ba178f72a7e
nazarovlex <collermix@gmail.com>
2023-02-09 16:29:22 +0300

    added TicTacToe class to tic_tac_toe.py

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,261 +1,272 @@
 import random
 import time
 
-while True:
-    try:
-        size = int(input("Input field size:\n"))
-        win_size = int(input("Input size of line to win:\n"))
-        while size < win_size:
-            print("Field size must be more then size of line ")
-            size = int(input("Input field size:\n"))
-            win_size = int(input("Input size of line to win:\n"))
-        break
-    except ValueError or IndexError:
-        print("Input correct numbers")
 
-field = [["□"] * size for i in range(size)]
+class TicTacToe:
+    def __init__(self, size, win_size, p1_name, p2_name):
+        self.size = size
+        self.win_size = win_size
+        self.p1_name = p1_name
+        self.p2_name = p2_name
 
+    def create_field(self):
+        field = [["□"] * self.size for _ in range(self.size)]
 
-def print_field():
-    for line in field:
-        for place in line:
-            if place == 0:
-                print("\033[31m{}".format(place), end="\t")
-            elif place == "X":
-                print("\033[34m{}".format(place), end="\t")
-            else:
-                print("\033[0m{}".format("□"), end="\t")
-        print("\n")
-    print("\033[0m".format())
+        return field
 
+    @staticmethod
+    def print_field(field):
+        for line in field:
+            for place in line:
+                if place == "0":
+                    print("\033[31m{}".format(place), end="\t")
+                elif place == "X":
+                    print("\033[34m{}".format(place), end="\t")
+                else:
+                    print("\033[0m{}".format("□"), end="\t")
+            print("\n")
+        print("\033[0m".format())
 
-def win_print(win_line):
-    for i in range(len(field)):
-        for j in range(len(field)):
-            if [i, j] in win_line:
-                print("\033[1;33m{}".format(field[i][j]), end="\t")
-            elif field[i][j] == 0:
-                print("\033[31m{}".format(field[i][j]), end="\t")
-            elif field[i][j] == "X":
-                print("\033[34m{}".format(field[i][j]), end="\t")
-            else:
-                print("\033[0m{}".format("□"), end="\t")
-        print("\n")
-    print("\033[0m".format())
+    @staticmethod
+    def win_print(field, win_line):
+        for i in range(len(field)):
+            for j in range(len(field)):
+                if [i, j] in win_line:
+                    print("\033[1;33m{}".format(field[i][j]), end="\t")
+                elif field[i][j] == "0":
+                    print("\033[31m{}".format(field[i][j]), end="\t")
+                elif field[i][j] == "X":
+                    print("\033[34m{}".format(field[i][j]), end="\t")
+                else:
+                    print("\033[0m{}".format("□"), end="\t")
+            print("\n")
+        print("\033[0m".format())
 
-
-def start():
-    win, win_line = str(), str()
-    mode, p1_name, p2_name = prompt()
-
-    if mode == 1:
-        win, win_line = player_vs_player(p1_name, p2_name)
-    elif mode == 2:
-        win, win_line = player_vs_bot(p1_name, p2_name)
-    elif mode == 3:
-        win, win_line = bot_vs_bot(p1_name, p2_name)
-
-    if win == "Tie":
-        print("Tie")
-        print_field()
-    else:
-        print(f"{win} WIN")
-        win_print(win_line)
-
-
-def player_vs_player(p1: str, p2: str) -> tuple[str, any]:
-    winner = str()
-    while True:
-        # player 1 turn
-        print_field()
-        print(p1 + " turn")
-        sign = "X"
-        player(sign)
-        win_check, win_line = check(sign)
-        if win_check == 1:
-            winner = "Tie"
-            break
-        elif win_check == 2:
-            winner = p1
-            break
-
-        # player 2 turn
-        print_field()
-        print(p2 + " turn")
-        sign = 0
-        player(sign)
-        win_check, win_line = check(sign)
-        if win_check == 1:
-            winner = "Tie"
-            break
-        elif win_check == 2:
-            winner = p2
-            break
-
-        continue
-    return winner, win_line
-
-
-def player_vs_bot(p: str, b: str) -> tuple[str, any]:
-    winner = str()
-    while True:
-        # player turn
-        print_field()
-        print(p + " turn")
-        sign = "X"
-        player(sign)
-        win_check, win_line = check(sign)
-        if win_check == 1:
-            winner = "Tie"
-            break
-        elif win_check == 2:
-            winner = p
-            break
-
-        # bot turn
-        print_field()
-        print(b + " turn")
-        sign = 0
-        bot(sign)
-        time.sleep(1)
-        win_check, win_line = check(sign)
-        if win_check == 1:
-            winner = "Tie"
-            break
-        elif win_check == 2:
-            winner = b
-            break
-
-        continue
-    return winner, win_line
-
-
-def bot_vs_bot(b1: str, b2: str) -> tuple[str, any]:
-    winner = str()
-    while True:
-        # bot#1 turn
-        print_field()
-        print(b1 + " turn")
-        sign = "X"
-        bot(sign)
-        time.sleep(1)
-        win_check, win_line = check(sign)
-        if win_check == 1:
-            winner = "Tie"
-            break
-        elif win_check == 2:
-            winner = b1
-            break
-
-        # bot#2 turn
-        print_field()
-        print(b2 + " turn")
-        sign = 0
-        bot(sign)
-        time.sleep(1)
-        win_check, win_line = check(sign)
-        if win_check == 1:
-            winner = "Tie"
-            break
-        elif win_check == 2:
-            winner = b2
-            break
-
-        continue
-    return winner, win_line
-
-
-def player(sign):
-    while True:
-        try:
-            y, x = input().split()
-            x = int(x) - 1
-            y = int(y) - 1
-            while field[size - 1 - x][y] != "□":
-                print("Input correct numbers(X and Y)")
+    @staticmethod
+    def player(field: [], sign: str, size: int):
+        while True:
+            try:
                 y, x = input().split()
                 x = int(x) - 1
                 y = int(y) - 1
-            field[size - 1 - x][y] = sign
-            break
-        except ValueError or IndexError:
-            print("Input 2 correct numbers: x and y")
 
+                while field[size - 1 - x][y] != "□" or not -1 < y < size or not -1 < x < size:
+                    print("Input correct numbers(X and Y)")
+                    y, x = input().split()
+                    x = int(x) - 1
+                    y = int(y) - 1
+                field[size - 1 - x][y] = sign
+                break
+            except (ValueError, IndexError):
+                print("Error: Input 2 correct numbers: x and y")
+        return field
 
-def bot(sign):
-    x = random.randint(0, size - 1)
-    y = random.randint(0, size - 1)
-    while field[x][y] != "□":
+    @staticmethod
+    def bot(field: [], sign: str, size: int):
         x = random.randint(0, size - 1)
         y = random.randint(0, size - 1)
-    field[x][y] = sign
+        while field[x][y] != "□":
+            x = random.randint(0, size - 1)
+            y = random.randint(0, size - 1)
+        field[x][y] = sign
+        return field
 
+    @staticmethod
+    def check(field: [], sign: str, win_size: int):
+        for line in range(len(field)):
+            for i in range(len(field) - (win_size - 1)):
+                cnt = 0
+                win_line = []
+                for j in range(i, win_size - 1 + i):
+                    if [line, j] not in win_line:
+                        win_line.append([line, j])
+                    if field[line][j] == field[line][j + 1] == sign:
+                        cnt += 1
+                        win_line.append([line, j + 1])
+                if cnt == win_size - 1:
+                    return 2, win_line
 
-def check(sign):
-    win_line = []
-    for line in range(len(field)):
+        for column in range(len(field)):
+            for i in range(len(field) - (win_size - 1)):
+                cnt = 0
+                win_line = []
+                for j in range(i, win_size - 1 + i):
+                    if [j, column] not in win_line:
+                        win_line.append([j, column])
+                    if field[j][column] == field[j + 1][column] == sign:
+                        cnt += 1
+                        win_line.append([j + 1, column])
+                if cnt == win_size - 1:
+                    return 2, win_line
+
         for i in range(len(field) - (win_size - 1)):
-            cnt = 0
-            win_line = []
-            for j in range(i, win_size - 1 + i):
-                if [line, j] not in win_line:
-                    win_line.append([line, j])
-                if field[line][j] == field[line][j + 1] == sign:
-                    cnt += 1
-                    win_line.append([line, j + 1])
-            if cnt == win_size - 1:
-                return 2, win_line
+            for j in range(len(field) - (win_size - 1)):
+                cnt = 0
+                win_line = []
+                for c in range(win_size - 1):
+                    if [i + c, j + c] not in win_line:
+                        win_line.append([i + c, j + c])
+                    if field[i + c][j + c] == field[i + c + 1][j + c + 1] == sign:
+                        cnt += 1
+                        win_line.append([i + c + 1, j + c + 1])
+                if cnt == win_size - 1:
+                    return 2, win_line
 
-    for column in range(len(field)):
-        for i in range(len(field) - (win_size - 1)):
-            cnt = 0
-            win_line = []
-            for j in range(i, win_size - 1 + i):
-                if [j, column] not in win_line:
-                    win_line.append([j, column])
-                if field[j][column] == field[j + 1][column] == sign:
-                    cnt += 1
-                    win_line.append([j + 1, column])
-            if cnt == win_size - 1:
-                return 2, win_line
+        for i in range(len(field) - win_size, len(field)):
+            for j in range(len(field) - (win_size - 1)):
+                cnt = 0
+                win_line = []
+                for c in range(win_size - 1):
+                    if [i - c, j + c] not in win_line:
+                        win_line.append([i - c, j + c])
+                    if field[i - c][j + c] == field[i - c - 1][j + c + 1] == sign and (i - c - 1) > -1:
+                        cnt += 1
+                        win_line.append([i - c - 1, j + c + 1])
+                if cnt == win_size - 1:
+                    return 2, win_line
 
-    for i in range(len(field) - (win_size - 1)):
-        for j in range(len(field) - (win_size - 1)):
-            cnt = 0
-            win_line = []
-            for c in range(win_size - 1):
-                if [i + c, j + c] not in win_line:
-                    win_line.append([i + c, j + c])
-                if field[i + c][j + c] == field[i + c + 1][j + c + 1] == sign:
+        cnt = 0
+        for i in range(len(field)):
+            for j in range(len(field)):
+                if field[i][j] == "□":
                     cnt += 1
-                    win_line.append([i + c + 1, j + c + 1])
-            if cnt == win_size - 1:
-                return 2, win_line
-
-    for i in range(len(field) - win_size, len(field)):
-        for j in range(len(field) - (win_size - 1)):
-            cnt = 0
-            win_line = []
-            for c in range(win_size - 1):
-                if [i - c, j + c] not in win_line:
-                    win_line.append([i - c, j + c])
-                if field[i - c][j + c] == field[i - c - 1][j + c + 1] == sign and (i-c-1)>-1:
-                    cnt += 1
-                    win_line.append([i - c - 1, j + c + 1])
-            if cnt == win_size - 1:
-                return 2, win_line
-
-    cnt = 0
-    for i in range(len(field)):
-        for j in range(len(field)):
-            if field[i][j] == "□":
-                cnt += 1
-    if cnt > 0:
+        if cnt > 0:
+            return 0, None
+        elif cnt == 0:
+            win_line = None
+            return 1, win_line
         return 0, None
-    elif cnt == 0:
-        win_line = None
-        return 1, win_line
-    return 0, None
+
+    def player_vs_player(self, field: [], win_size: int, p1: str, p2: str) -> tuple[str, any]:
+        winner = str()
+        while True:
+            # player 1 turn
+            self.print_field(field)
+            print(p1 + " turn")
+            sign = "X"
+            field = self.player(field, sign, len(field))
+            win_check, win_line = self.check(field, sign, win_size)
+            if win_check == 1:
+                winner = "Tie"
+                break
+            elif win_check == 2:
+                winner = p1
+                break
+
+            # player 2 turn
+            self.print_field(field)
+            print(p2 + " turn")
+            sign = "0"
+            field = self.player(field, sign, len(field))
+            win_check, win_line = self.check(field, sign, win_size)
+            if win_check == 1:
+                winner = "Tie"
+                break
+            elif win_check == 2:
+                winner = p2
+                break
+
+            continue
+        return winner, win_line
+
+    def player_vs_bot(self, field: [], win_size: int, p: str, b: str) -> tuple[str, any]:
+        winner = str()
+        while True:
+            # player turn
+            self.print_field(field)
+            print(p + " turn")
+            sign = "X"
+            field = self.player(field, sign, len(field))
+            win_check, win_line = self.check(field, sign, win_size)
+            if win_check == 1:
+                winner = "Tie"
+                break
+            elif win_check == 2:
+                winner = p
+                break
+
+            # bot turn
+            self.print_field(field)
+            print(b + " turn")
+            sign = "0"
+            field = self.bot(field, sign, len(field))
+            time.sleep(1)
+            win_check, win_line = self.check(field, sign, win_size)
+            if win_check == 1:
+                winner = "Tie"
+                break
+            elif win_check == 2:
+                winner = b
+                break
+
+            continue
+        return winner, win_line
+
+    def bot_vs_bot(self, field: [], win_size: int, b1: str, b2: str) -> tuple[str, any]:
+        winner = str()
+        while True:
+            # bot#1 turn
+            self.print_field(field)
+            print(b1 + " turn")
+            sign = "X"
+            field = self.bot(field, sign, len(field))
+            time.sleep(1)
+            win_check, win_line = self.check(field, sign, win_size)
+            if win_check == 1:
+                winner = "Tie"
+                break
+            elif win_check == 2:
+                winner = b1
+                break
+
+            # bot#2 turn
+            self.print_field(field)
+            print(b2 + " turn")
+            sign = "0"
+            field = self.bot(field, sign, len(field))
+            time.sleep(1)
+            win_check, win_line = self.check(field, sign, win_size)
+            if win_check == 1:
+                winner = "Tie"
+                break
+            elif win_check == 2:
+                winner = b2
+                break
+
+            continue
+        return winner, win_line
+
+
+def start():
+    while True:
+        try:
+            size = int(input("Input field size:\n"))
+            win_size = int(input("Input size of line to win:\n"))
+            while size < win_size:
+                print("Field size must be more then size of line ")
+                size = int(input("Input field size:\n"))
+                win_size = int(input("Input size of line to win:\n"))
+            break
+        except ValueError or IndexError:
+            print("Input correct numbers")
+    win, win_line = str(), str()
+    mode, p1_name, p2_name = prompt()
+    game = TicTacToe(size, win_size, p1_name, p2_name)
+    field = game.create_field()
+    game.print_field(field)
+    if mode == 1:
+        win, win_line = game.player_vs_player(field, win_size, p1_name, p2_name)
+    elif mode == 2:
+        win, win_line = game.player_vs_bot(field, win_size, p1_name, p2_name)
+    elif mode == 3:
+        win, win_line = game.bot_vs_bot(field, win_size, p1_name, p2_name)
+
+    if win == "Tie":
+        print("Tie")
+        game.print_field(field)
+    else:
+        print(f"{win} WIN")
+        game.win_print(field, win_line)
 
 
 def prompt():
@@ -280,3 +291,9 @@ def prompt():
                 return mode, "Bot#1", "Bot#2"
         except ValueError or IndexError:
             print("Input correct numbers")
+
+
+
+
+
+
